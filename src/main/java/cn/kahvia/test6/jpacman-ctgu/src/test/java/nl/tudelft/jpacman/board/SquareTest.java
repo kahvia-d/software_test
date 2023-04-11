b8f14ab2aff4d9e0aa5555ccf1eb760997dc22d5
kahvia-d <xuedongtan@163.com>
2023-04-11 18:53:27 +0800

    add test6.

---
+++ src/main/java/cn/kahvia/test6/jpacman-ctgu/src/test/java/nl/tudelft/jpacman/board/SquareTest.java
@@ -0,0 +1,63 @@
+package nl.tudelft.jpacman.board;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+
+/**
+ * Test suite to confirm the public API of {@link Square} works as desired.
+ *
+ * @author Jeroen Roosen 
+ */
+class SquareTest {
+
+    /**
+     * The square under test.
+     */
+    private Square square;
+
+    /**
+     * Resets the square under test.
+     */
+    @BeforeEach
+    void setUp() {
+        square = new BasicSquare();
+    }
+
+
+    //未占据位置
+    @Test
+    void noStartSquare(){
+        Unit occupant = mock(Unit.class);
+
+        assertThat(occupant.getSquare()).isNull();
+    }
+
+
+    //占据一次位置
+    @Test
+    void testOccupy() {
+        Unit occupant = spy(Unit.class);
+        //尝试占据空间
+        occupant.occupy(square);
+        assertThat(square.getOccupants()).contains(occupant);
+    }
+
+
+    //重复占据同一位置
+    @Test
+    void testReoccupy() {
+        Unit occupant = spy(Unit.class);
+        occupant.occupy(square);
+        occupant.occupy(square);
+
+        assertThat(square.getOccupants()).contains(occupant);
+    }
+
+
+
+
+}
